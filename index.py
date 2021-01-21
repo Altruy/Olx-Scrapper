433c60ddec15d672cef21f8a4def82663e8e401e
Muhammad Haroon <m_haroon96@hotmail.com>
2021-01-21 23:55:43 +0500

    added int

--- index.py
+++ index.py
@@ -19,4 +19,4 @@ def download(filename):
         
         
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
