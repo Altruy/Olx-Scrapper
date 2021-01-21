6f225134a9edd09257a820caae501dd082dd8613
Muhammad Haroon <m_haroon96@hotmail.com>
2021-01-21 23:55:11 +0500

    added webui

---
+++ index.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_file, abort
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    crawls = [i for i in os.listdir('crawls') if i.endswith('.csv')]
+    return render_template('index.html', crawls=crawls)
+
+
+@app.route('/download/<filename>')
+def download(filename):
+    filepath = os.path.join('crawls', filename)
+    if not os.path.exists(filepath):
+        abort(404)
+        
+    return send_file(filepath)
+        
+        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
