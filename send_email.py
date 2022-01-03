dbe71b09b9f37137e5f2a54153657a543884771a
Altruy <turyal.neeshat5@gmail.com>
2022-01-03 20:06:54 +0500

    for github

--- send_email.py
+++ send_email.py
@@ -15,21 +15,22 @@ def send_email(email_recipient, email_subject, email_message, attachments=[]):
     msg['Subject'] = email_subject
 
     msg.attach(MIMEText(email_message, 'plain'))
-    
+
     for attachment_location in attachments:
         filename = os.path.basename(attachment_location)
         attachment = open(attachment_location, "rb")
         part = MIMEBase('application', 'octet-stream')
         part.set_payload(attachment.read())
         encoders.encode_base64(part)
-        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+        part.add_header('Content-Disposition',
+                        "attachment; filename= %s" % filename)
         msg.attach(part)
-    
+
     try:
         server = smtplib.SMTP('smtp.office365.com', 587)
         server.ehlo()
         server.starttls()
-        server.login('pkcrawler2021@outlook.com', 'ZzPJ6wwRS3iREc3')
+        server.login('pkcrawler2021@outlook.com', "-")
         text = msg.as_string()
         server.sendmail(email_sender, email_recipient, text)
         print('email sent')
